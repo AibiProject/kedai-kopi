f9f898d0637f638a08384b521f0db76bf99db1b2
AibiProject <zakizira0305@gmail.com>
2023-11-12 09:15:27 +0300

    membuat website kedai kopi

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+// Klik untuk menghilangkan sidebar
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+  if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
